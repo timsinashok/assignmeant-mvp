a412824026c4597831a5bdd87c1f819d08504329
Ashok Timsina <ashoktimsina147181@gmail.com>
2024-07-17 23:25:04 +0400

    backend: updated models to adher to flask login

--- assignmeant_app/models.py
+++ assignmeant_app/models.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 # Base User class
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
